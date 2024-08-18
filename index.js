925181a0744c15c12116a28ec36ca46de4154c15
Mahidun Nobi <mdmahidunnobi@gmail.com>
2024-08-18 09:33:44 +0600

    Add: Property Model

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const fs = require("fs");
 const mongoose = require("mongoose");
-const path = "./115_111_01.BLM";
+const path = "./up/115_111_01.BLM";
 
 const app = express();
 const port = process.env.PORT || 5000;
