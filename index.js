3018e282a3a9d7c551721f48df5786e127ace82c
Mahidun Nobi <mdmahidunnobi@gmail.com>
2024-08-28 16:41:16 +0600

    Add: Automatic Data Restore system

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const propertyRouter = require("./routers/propertyRouter.js");
 const path = require("path");
 const nodemailer = require("nodemailer");
 const Handlebars = require("handlebars");
+const cron = require("node-cron");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -222,11 +223,57 @@ app.get("/restore-data", (req, res) => {
       })
       .filter((record) => record !== null);
 
-    const properties = new PropertyModel(records);
-    await properties.save();
+    const deleteRes = await PropertyModel.deleteMany();
+    const properties = await PropertyModel.insertMany(records);
 
     // return "Parsed Data:", records.slice(0, 5); // Display first 5 records
-    return res.json(records);
+    return res.json({ message: "Data Restored" });
+  });
+});
+
+// Automatically data will be restored in 12:00 AM everyday
+cron.schedule("0 0 * * *", () => {
+  const data = fs.readFile(blmPath, "utf8", async (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return res.json({ message: "Something went wrong..!" });
+    }
+
+    // Split file content by lines
+    const lines = data.split("\n");
+    //   console.log("First 10 lines:", lines.slice(0, 10));
+
+    // Assuming metadata is within the first few lines
+    const metadata = lines.slice(0, 5);
+
+    // Find the line starting with '#DEFINITION#' and extract columns
+    const definitionIndex = lines.findIndex((line) =>
+      line.includes("#DEFINITION#")
+    );
+    const columns = lines[definitionIndex + 1].trim().split("^");
+    // //   console.log("Columns:", columns);
+
+    // // ------Getting the actual data
+    const dataStartIndex =
+      lines.findIndex((line) => line.includes("#DATA#")) + 1;
+    const dataLines = lines.slice(dataStartIndex);
+
+    const records = dataLines
+      .map((line) => {
+        if (line.trim() === "") return null;
+        const fields = line.trim().split("^");
+        return fields.reduce((obj, field, index) => {
+          obj[columns[index]] = field;
+          return obj;
+        }, {});
+      })
+      .filter((record) => record !== null);
+
+    const deleteRes = await PropertyModel.deleteMany();
+    const properties = await PropertyModel.insertMany(records);
+
+    // return "Parsed Data:", records.slice(0, 5); // Display first 5 records
+    console.log("Data Restored");
   });
 });
 
