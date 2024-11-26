c1bcf6a86f4b3e1d125a458c7c916cc355bd19a7
MahidunNobi <mdmahidunnobi@gmail.com>
2024-11-26 22:36:15 +0600

    template4.html - try 02

--- utiles/priceReductionCheck.js
+++ utiles/priceReductionCheck.js
@@ -10,17 +10,16 @@ Handlebars.registerHelper("increment", function (index) {
   return index + 1;
 });
 
-// Setting the email template
-const source = fs
-  .readFileSync("../email-templates/template4.html", "utf-8")
-  .toString();
-const template = Handlebars.compile(source);
-
 const priceReductionCheck = async () => {
   const users = await User.find();
   if (users.length < 1) {
     return;
   }
+  // Setting the email template
+  const source = fs
+    .readFileSync("../email-templates/template4.html", "utf-8")
+    .toString();
+  const template = Handlebars.compile(source);
 
   await Promise.all(
     users.map(async (user) => {
