3fb5374afbba42c8f404ec38b726bdc25460056a
Maulik Umrania <maulikumrania@gmail.com>
2024-12-02 18:45:52 +0530

    DB Change to new account

--- utiles/dbConnect.js
+++ utiles/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.cxk7yn6.mongodb.net/haus?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@haus.61i55.mongodb.net/?retryWrites=true&w=majority&appName=haus`
     );
     console.log("Connected with the database successfully");
   } catch (error) {
