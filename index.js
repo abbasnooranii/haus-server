98253d6a33669ffbe18afa4b6136a3e6c38315e7
Maulik Umrania <maulikumrania@gmail.com>
2024-12-03 19:48:36 +0530

    Move FTP download file to tmp folder

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ async function downloadFilesFromFTP() {
     password: "6Ws0BVCHlO5F",
     secure: false,
   };
-  const localDirectory = "./up/";
+  const localDirectory = "/tmp/";
   const client = new ftp.Client();
 
   try {
