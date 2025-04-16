503d891749553ba50ee6ef2786f39d31db3de62d
Abbas <abbasnoorani93@gmail.com>
2025-04-16 13:39:22 +0530

    property search changes document 2

--- utiles/getFilterObj.js
+++ utiles/getFilterObj.js
@@ -1,14 +1,41 @@
 const getFilterObj = (query) => {
-  // console.log(query)
+  console.log(query)
   const filter = {};
   if(query.showLetAgreed != 'true')
     filter.STATUS_ID = { $nin: [5] }; // default filter
   if (query.agent_ref) {
     filter.AGENT_REF = new RegExp(query.agent_ref, "i");
   }
+    /*
   if (query.badrooms) {
     const bedroomsArray = query.badrooms.split(",").map(Number); // Convert the "4,5,7,8" string into an array of numbers
     filter.BEDROOMS = { $in: bedroomsArray };
+  }
+    */
+  if (query.badrooms) {
+    const bedroomsArray = query.badrooms.split(",").map(Number);
+    if (bedroomsArray.includes(11)) {
+      const filteredArray = bedroomsArray.filter(num => num !== 11);
+      
+      if (filteredArray.length > 0) {
+        filter.$or = [
+          { BEDROOMS: { $in: filteredArray.map(num => num.toString()) } },
+          { 
+            $and: [
+              { BEDROOMS: { $regex: "^\\d+$" } },  
+              { $expr: { $gte: [{ $toInt: "$BEDROOMS" }, 11] } }
+            ]
+          }
+        ];
+      } else {
+        filter.$and = [
+          { BEDROOMS: { $regex: "^\\d+$" } }, 
+          { $expr: { $gte: [{ $toInt: "$BEDROOMS" }, 11] } }
+        ];
+      }
+    } else {
+      filter.BEDROOMS = { $in: bedroomsArray.map(num => num.toString()) };
+    }
   }
   if (query.max_price || query.min_price) {
     filter.$expr = {
@@ -17,7 +44,7 @@ const getFilterObj = (query) => {
         {
           $lte: [
             { $toDouble: "$PRICE" },
-            query.max_price === "0" ? 2000 : Number(query.max_price),
+            query.max_price === "0" ? 200 : Number(query.max_price),
           ],
         },
       ],
